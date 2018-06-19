4e50465530681b9851f492473d68a672e0ce2fcc
Jakob Wegenschimmel <jwegenschimmel@rzl.at>
2018-06-19 09:09:28 +0200

    Configure DbContext

--- EfCoreDeadlockTest/MyContext.cs
+++ EfCoreDeadlockTest/MyContext.cs
@@ -6,5 +6,8 @@ namespace EfCoreDeadlockTest
     {
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer(@"Server=.;Database=ReproDeadlock;Trusted_Connection=True;MultipleActiveResultSets=True");
     }
 }
